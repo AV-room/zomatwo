db730e33915b1c7072a58c8d2818afc196814a49
An Vu <anvu@Ans-MacBook-Pro.local>
2020-10-12 02:55:32 +1030

    add more detail to results list

--- src/components/ScrollableList/index.tsx
+++ src/components/ScrollableList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles.scss';
 import Restaurant from '../../interfaces/Restaurant';
+import { convertPriceBracketToSymbol } from '../../utils/helpers';
 
 type ScrollableListProps = { list: Restaurant[]; handleSelection: Function };
 
@@ -8,19 +9,21 @@ const ScrollableList: React.FC<ScrollableListProps> = ({
   list,
   handleSelection
 }) => {
-  const names = list && list.map((r) => r.name);
-
   return (
     <div className="results-list">
       <h2>Results ({list.length})</h2>
       <ul>
-        {list.map((r: any, i: number) => {
+        {list.map((r: Restaurant, i: number) => {
           return (
             <li key={i} onClick={(e) => handleSelection(i)}>
               {r.name}
-              {/* <br />
-              <em>{r.cuisines}</em> <br />${r.average_cost_for_two},{' '}
-              {r.user_rating.aggregate_rating} */}
+              <div className="quick-bits">
+                <span>{r.cuisines}</span> <br />
+                <span>
+                  {convertPriceBracketToSymbol(r.price_range)} |{' '}
+                  {r.user_rating.aggregate_rating}
+                </span>
+              </div>
             </li>
           );
         })}
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,2 +1,11 @@
 export const withinRange = (range: number[], value: number) =>
   value >= range[0] && value <= range[1];
+
+export const convertPriceBracketToSymbol = (priceBracket: number) => {
+  let symbolString = '';
+  for (let i = 0; i < priceBracket; i++) {
+    symbolString = `${symbolString}$`;
+  }
+
+  return symbolString;
+};
