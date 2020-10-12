51fc2d377c8136c14e3eff90248f1a309e89f8bd
An Vu <anvu@Ans-MacBook-Pro.local>
2020-10-12 22:22:00 +1030

    Updates

---
+++ precache-manifest.e1209b4645c00c41155f46c5b33c4ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a002ebb781a419c352b5172aa3b6775f",
+    "url": "/zomatwo/index.html"
+  },
+  {
+    "revision": "c687e5f3850ebef8f7f3",
+    "url": "/zomatwo/static/css/main.bda88f8a.chunk.css"
+  },
+  {
+    "revision": "c9de2fafef93023ab15c",
+    "url": "/zomatwo/static/js/2.5b719975.chunk.js"
+  },
+  {
+    "revision": "7c916f72723944718cac497dcc1054f3",
+    "url": "/zomatwo/static/js/2.5b719975.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c687e5f3850ebef8f7f3",
+    "url": "/zomatwo/static/js/main.fe7a2bea.chunk.js"
+  },
+  {
+    "revision": "6967aaba641cfa99d152",
+    "url": "/zomatwo/static/js/runtime-main.7a591f66.js"
+  }
+]);
